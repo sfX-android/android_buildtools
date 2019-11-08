998968838f723520b7c02b39b91b9068c43e236e
steadfasterX <steadfasterX@gmail.com>
2019-11-08 10:08:23 +0100

    improve builder

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -7,9 +7,13 @@
 
 echo "starting build wrapper $0 ..."
 SRCPATH="$1"
-RUNCMD="$2"
+RUNCMD="$@"
 
-export CCACHE_DIR=/home/androidsource/jenkins/.ccache
+
+source ~/.profile
+source ~/.bashrc
+
+[ -z "$CCACHE_DIR" ] && export CCACHE_DIR=/ccache/$USER
 
 echo -e "USE_CCACHE: $USE_CCACHE\nCCACHE_DIR: $CCACHE_DIR\nANDROID_JACK_VM_ARGS: $ANDROID_JACK_VM_ARGS\nSRCPATH: $SRCPATH\nRUNCMD: $RUNCMD"
 
@@ -22,6 +26,18 @@ f_help(){
 
 cd $SRCPATH
 source build/envsetup.sh
-$RUNCMD
 
-echo "build wrapper $0 finished..."
+echo -e "ro.product.cpu.abilist=$TARGET_CPU_ABI,$TARGET_2ND_CPU_ABI"
+
+[ "$3" == "killjack" ] && jack-admin kill-server
+
+# filter out unwanted stuff
+RUNCMD=$(echo "$RUNCMD" |grep -vo killjack | grep -vo "$SRCPATH")
+
+$RUNCMD
+RET=$?
+
+echo -e "ro.product.cpu.abilist=$TARGET_CPU_ABI,$TARGET_2ND_CPU_ABI"
+
+echo "build wrapper $0 finished with $RET"
+exit $RET
