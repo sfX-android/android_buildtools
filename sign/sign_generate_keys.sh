d1e4d02eae34e87b395aeced41eb15df91024121
steadfasterX <steadfasterX@gmail.com>
2022-12-12 15:46:18 +0000

    optimize key gen
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

--- sign/sign_generate_keys.sh
+++ sign/sign_generate_keys.sh
@@ -3,11 +3,26 @@
 VENDOR_DIR=$(dirname $0)
 [ -z "$KEYS_DIR" ] && KEYS_DIR=user-keys
 _USR=$USER_NAME
+[ -z $_USR ] && _USR=$BUILD_USERNAME
 [ -z $_USR ] && echo "ERROR: missing USER_NAME var!" && exit 3
 
-KEYS_SUBJECT='/C=DE/ST=Somewhere/L=Somewhere/O='${_USR}'/OU=e/CN=eOS/emailAddress=android@android.local'
+# default key/hash sizes
+DEFKSIZE=4096
+DEFHASHTYPE=sha256
+
+KEYS_SUBJECT='/C=DE/ST=Somewhere/L=Somewhere/CN='${_USR}'/OU=aosp/O=android/emailAddress=android@android.local'
 
 [ ! -d $KEYS_DIR ] && mkdir -p $KEYS_DIR
+
+case $KSIZE in
+    4096|8192) echo setting rsa-${KSIZE} ;;
+    *) echo -e "UNSUPPORTED or empty key size: >$KSIZE<\nUsing default KSIZE=$DEFKSIZE"; export KSIZE=$DEFKSIZE;;
+esac
+
+case $HASHTYPE in
+    sha256|sha512) echo setting $HASHTYPE ;;
+    *) echo -e "UNSUPPORTED or empty hash >$HASHTYPE<\Using default HASHTYPE=$DEFHASHTYPE" ; export HASHTYPE=$DEFHASHTYPE ;;
+esac
 
 for c in releasekey platform shared media networkstack verity sdk_sandbox bluetooth; do
     for k in pem key pk8 x509.pem der;do
@@ -27,10 +42,19 @@ for c in cyngn{-priv,}-app testkey; do
 done
 
 # make readable format for manual verifier
-openssl rsa -inform DER -outform PEM -in $KEYS_DIR/releasekey.pk8 -out $KEYS_DIR/releasekey.pem
-openssl rsa -in $KEYS_DIR/releasekey.pem -pubout > $KEYS_DIR/releasekey.pub
+[ ! -f "$KEYS_DIR/releasekey.pem" ] && openssl rsa -inform DER -outform PEM -in $KEYS_DIR/releasekey.pk8 -out $KEYS_DIR/releasekey.pem && echo "... $KEYS_DIR/releasekey.pem created"
+[ ! -f "$KEYS_DIR/releasekey.pub" ] && openssl rsa -in $KEYS_DIR/releasekey.pem -pubout > $KEYS_DIR/releasekey.pub && echo "... $KEYS_DIR/releasekey.pub created"
 
 # make AVB required stuff
-openssl x509 -outform DER -in $KEYS_DIR/releasekey.x509.pem -out $KEYS_DIR/releasekey.x509.der
-openssl pkcs8 -in $KEYS_DIR/releasekey.pk8 -inform DER -out $KEYS_DIR/releasekey.key -nocrypt
-external/avb/avbtool extract_public_key --key $KEYS_DIR/releasekey.key --output $KEYS_DIR/pkmd.key
+# AVB supports usually (atm) only sha256+4096bit max
+for a in pk8;do
+    if [ -f "$KEYS_DIR/avb.${a}" ];then
+	echo "WARNING: avb.${a} exists!! I WILL NOT OVERWRITE EXISTING KEYS!"
+	continue 2
+    fi
+    echo ">> [$(date)] Generating AVB ($a)..."
+    export KSIZE=4096 HASHTYPE=sha256 ; ${VENDOR_DIR}/make_key "$KEYS_DIR/avb" "$KEYS_SUBJECT" <<< '' #&> /dev/null
+done
+[ ! -f "$KEYS_DIR/avb.x509.der" ] && openssl x509 -outform DER -in $KEYS_DIR/avb.x509.pem -out $KEYS_DIR/avb.x509.der && echo "... $KEYS_DIR/avb.x509.der created"
+[ ! -f "$KEYS_DIR/avb.key" ] && openssl pkcs8 -in $KEYS_DIR/avb.pk8 -inform DER -out $KEYS_DIR/avb.key -nocrypt && echo "... $KEYS_DIR/avb.key created"
+[ ! -f "$KEYS_DIR/avb.pem" ] && external/avb/avbtool extract_public_key --key $KEYS_DIR/avb.key --output $KEYS_DIR/avb.pem && echo "... $KEYS_DIR/avb.pem created"
