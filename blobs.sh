bf21b3adb637d65a85ba8307519b682374a53f9a
steadfasterX <steadfasterX@users.noreply.github.com>
2023-08-29 09:43:34 +0200

    Update blobs.sh

--- blobs.sh
+++ blobs.sh
@@ -1,42 +1,41 @@
 #!/bin/bash
 ###########################################################################################
 #
-# find blob deps!
+# parses a blob and prints all its dependencies - and dependencies of those, too
 #
-# Copyright (2020): steadfasterX <steadfasterX | gmail - com>
+# Copyright 2020-2023: steadfasterX <steadfasterX | gmail - com>
 #
 ###########################################################################################
 
+[ -z "$DEBUG" ] && DEBUG=0
+spath="$1"
+FOUND="$2"
+BLOBS=()
+
+if [ "$spath" ]||[ -f "$FOUND" ];then echo "ERROR. usage: $0 <search path> <full-path-to-blob>"; exit 4;fi
+
 lib_lookup() {
-
-	readelf -d $1 | grep NEEDED | cut -d "[" -f2 | cut -d"]" -f 1
-	#readelf -d $1 | awk '/NEEDED/ {print $5}' | sed -e 's/\[\|\]//g'
-
+    readelf -d $1 | grep NEEDED | cut -d "[" -f2 | cut -d"]" -f 1
 }
 
 so_search() {
-	for file in `lib_lookup $1`; do
-		find . -iname "$file" 2>/dev/null
-	done
+    for file in `lib_lookup $1`; do
+        find $spath -iname "$file" 2>/dev/null
+    done
 }
 
 global_search() {
-	RESULT=$(so_search $1 | egrep -v "^${FOUND}" | grep -v "libc.so\|libdl.so\|libc++.so\|libm.so\|liblog.so\|libcutils.so")
-	for blob in `echo -n $RESULT`; do
-                CNT=$((CNT+1))
-		echo $blob
-		FOUND=$1\|$FOUND
-                BLOBS[1]+="$blob " 
-		global_search $blob
-                [ $CNT -gt 20 ] && break
-	done
+    RESULT=$(so_search $1 | grep -Ev "^${FOUND}" | grep -v "libc.so\|libdl.so\|libc++.so\|libm.so\|liblog.so\|libcutils.so")
+    for blob in `echo -n $RESULT`; do
+        CNT=$((CNT+1))
+        echo $blob
+        FOUND=$1\|$FOUND
+        BLOBS[1]+="$blob "
+        global_search $blob
+        [ $CNT -gt 20 ] && break
+    done
 }
 
-FOUND=$1
-BLOBS=()
-
-echo starting search on $1
-
-global_search $1
-
-echo finished search on $1
+[ "$DEBUG" -eq 1 ] && echo starting search on $1
+global_search $FOUND
+[ "$DEBUG" -eq 1 ] && echo finished search on $1
